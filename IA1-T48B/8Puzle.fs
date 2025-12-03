181933f778644bde5392b8321bbf5b135e01ad4f
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-12-03 16:46:54 -0600

    test

---
+++ IA1-T48B/8Puzle.fs
@@ -0,0 +1,12 @@
+(*
+Resolver el 8-Puzzle. El estado es una lista de 9 enteros (0 representa el espacio vacío).
+Estado Meta: [0; 1; 2; 3; 4; 5; 6; 7; 8]
+Heurística: Número de piezas que no están en su posición correcta (sin contar el espacio vacío 0).
+Implemente resolverPuzzle: int list -> string list. usando A* donde la entrada es el estado inicial y 
+    la salida es la lista de acciones representadas como strings ("Up", "Down", "Left", "Right").
+
+*)
+
+module Puzle
+
+list enteos = []
